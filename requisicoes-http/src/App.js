fbcebc68c4fa9bff22fe6e2cacb072f6365799e7
mathznxs <math945374081@gmail.com>
2026-04-28 11:47:51 -0300

    S2A11 - Requisições HTTP: Alimentamos um blog utilizando o conteúdo de uma API

---
+++ requisicoes-http/src/App.js
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from 'react'  
+import './style.css'
+
+// https://sujeitoprogramador.com/rn-api/?api=posts
+
+function App() {
+
+  const [nutri, setNutri] = useState([])
+
+  useEffect(() => {
+    function loadApi() {
+      let url = 'https://sujeitoprogramador.com/rn-api/?api=posts'
+      fetch(url)
+      .then((r) => r.json())
+      .then((json) => {
+        setNutri(json)
+      })
+    }
+    loadApi()
+  }, [])
+
+  return (
+    <div className="container">
+      <header className="">
+        <strong className="">React Nutri</strong>
+      </header>
+      {nutri.map((item) => {
+        return (
+          <article key={item.id} className='post'>
+            <strong className='titulo'>{item.titulo}</strong>
+
+            <img src={item.capa} alt={item.titulo} className="capa" />
+
+            <p className="subtitulo">{item.subtitulo}</p>
+            <h1 className="">Categoria: {item.categoria}</h1>
+            <a href="" className="botao">Acessar </a>
+          </article>
+        )
+      })}
+    </div>
+  );
+}
+
+export default App;
