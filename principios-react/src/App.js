3910bfaef71a9ebedd02d2a5f32d3fb67aafa553
mathznxs <math945374081@gmail.com>
2026-04-28 10:59:40 -0300

    S2A10 - useEffect

--- principios-react/src/App.js
+++ principios-react/src/App.js
@@ -1,65 +1,75 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [task, setTask] = useState([]);
   const [input, setInput] = useState("");
-  const [editingTask, setEditingTask] = useState(null);
+  const [tarefa, setTarefa] = useState(() => {
+    const storage = localStorage.getItem("@tarefas");
+
+    if (storage) {
+      try {
+        return JSON.parse(storage);
+      } catch {
+        return [];
+      }
+    }
+
+    return [
+      { id: 1, nome: "Estudar React" },
+      { id: 2, nome: "Pagar conta de luz" },
+    ];
+  });
+
+  useEffect(() => {
+    const tarefasStorage = localStorage.getItem("@tarefas");
+
+    if (tarefasStorage) {
+      setTarefa(JSON.parse(tarefasStorage));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("@tarefas", JSON.stringify(tarefa));
+  }, [tarefa]);
 
   function handleRegister(e) {
     e.preventDefault();
 
-    if (!input.trim()) return;
+    const newTask = {
+      id: Date.now(),
+      nome: input,
+    };
 
-    if (editingTask) {
-      setTask((prev) =>
-        prev.map((item) =>
-          item.id === editingTask.id ? { ...item, nome: input } : item,
-        ),
-      );
-      setEditingTask(null);
-    } else {
-      const newTask = {
-        id: Date.now(),
-        nome: input,
-      };  
+    setTarefa((prev) => [...prev, newTask]);
 
-      setTask((prev) => [...prev, newTask]);
-    }
     setInput("");
   }
 
   function handleDeleteTask(id) {
-    setTask((prev) => prev.filter((item) => item.id !== id));
+    setTarefa((prev) => prev.filter((item) => item.id !== id));
   }
 
   return (
     <div>
-      <h1>Cadastrando Tarefas</h1>
-
-      <form onSubmit={handleRegister} className="form">
-        <div className="input-task">
-          <input
-            placeholder="Digite uma tarefa"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-          />
-        </div>
-        <button type="submit">{editingTask ? "Salvar" : "Cadastrar"}</button>
+      <h1>Cadastrando usuários</h1>
+      <form onSubmit={handleRegister} className="forms">
+        <br /> <label>Nome da tarefa: </label>
+        <input
+          placeholder="Digite uma tarefa"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <br /> <br />
+        <button type="submit" className="button-forms">
+          Cadastrar tarefa
+        </button>
       </form>
-
-      <div>
-        <ul className="task-list">
-          {task.map((item) => (
-            <div key={item.id}>
+      <div className="users">
+        <ul>
+          {tarefa.map((item) => (
+            <div key={item.id} className="task">
               <li>{item.nome}</li>
-              <button onClick={() => handleDeleteTask(item.id)}>Deletar</button>
-              <button
-                onClick={() => {
-                  setEditingTask(item);
-                  setInput(item.nome);
-                }}
-              >
-                Editar
+              <button onClick={() => handleDeleteTask(item.id)}>
+                Deletar tarefa
               </button>
             </div>
           ))}
